71337f5c05f2bd213cf6eb90184c48412f185a21
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-02-26 17:31:11 +0530

    Downward Triangle Pattern in Java

--- src/DownwardTrianglePattern.java
+++ src/DownwardTrianglePattern.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class DownwardTrianglePattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int star = scanner.nextInt();
+        int initialStar = 0;
+        int space = 0;
+        for(int i = 0; i < star; i++){
+            for (int j=0; j<space; j++){
+                System.out.print(" ");
+            }
+            for (int j = initialStar; j < star; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            initialStar++;
+            space++;
+        }
+    }
 }
