67e8f632b5e2dd13fb2ec7828fcaa0663f4bd6e8
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-02-27 21:45:17 +0530

    PatternMagic in java

--- src/PatternMagic.java
+++ src/PatternMagic.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Created by Vasundhara Tiwari
  * On 26-02-2022 at 17:57
  */
 
 public class PatternMagic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rows = scanner.nextInt();
+
+    }
 }
